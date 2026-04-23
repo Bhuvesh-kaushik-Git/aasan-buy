d83cb03e90fa6a31bf73bc0c766fc0684e3621ff
pratham-b4g <pratham.arora@biz4group.com>
2026-04-23 17:11:31 +0530

    chore: remove temporary diagnostic script

--- backend/test-env.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config();
-console.log('MONGO_URI:', process.env.MONGO_URI ? 'SET' : 'MISSING');
-console.log('PORT:', process.env.PORT);
-console.log('RAZORPAY_KEY_ID:', process.env.RAZORPAY_KEY_ID ? 'SET' : 'MISSING');
-console.log('RAZORPAY_KEY_SECRET:', process.env.RAZORPAY_KEY_SECRET ? 'SET' : 'MISSING');
